7d4177284dbb05cd52f3ffe963bb453d00fc699b
Herp Derpinstine <herpderpinstine@gmail.com>
2020-09-05 13:51:06 -0500

    Add files via upload

--- RememberMe.cs
+++ RememberMe.cs
@@ -13,7 +13,7 @@ namespace RememberMe
         public const string Name = "RememberMe";
         public const string Author = "Herp Derpinstine";
         public const string Company = "Lava Gang";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string DownloadLink = "https://github.com/HerpDerpinstine/RememberMe";
     }
 
